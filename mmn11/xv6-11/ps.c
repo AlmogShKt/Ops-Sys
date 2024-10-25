fd55fc15ef809a6d76392c08d4a6d9a63f47d90a
Almog Shtaigmann <a.shtaigmann@gmail.com>
2024-10-25 17:22:47 +0300

    mmn11.v1.0.0.2

---
+++ mmn11/xv6-11/ps.c
@@ -0,0 +1,12 @@
+// Name Almog Shtaigmann:  ID: 209401553
+
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+int
+main(int argc, char *argv[]) {
+    cps153();
+    exit();
+}
--- mmn11/xv6-11/user.h
+++ mmn11/xv6-11/user.h
@@ -23,6 +23,7 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int ps153(void);
 
 // ulib.c
 int stat(const char*, struct stat*);
