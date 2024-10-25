f1cb4cc23507f0e5b8fbf1a2e37a62d647393279
Almog Shtaigmann <a.shtaigmann@gmail.com>
2024-10-25 17:56:08 +0300

    mmn11.v1.0.0.6

--- mmn11/xv6-11/defs.h
+++ mmn11/xv6-11/defs.h
@@ -120,7 +120,7 @@ void            userinit(void);
 int             wait(void);
 void            wakeup(void*);
 void            yield(void);
-void            cps153(void);
+int            cps153(void);
 
 // swtch.S
 void            swtch(struct context**, struct context*);
